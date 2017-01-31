c5bc5f98f324fa9aba9679148670eb97527225b3
Walfie <walfington@gmail.com>
2017-01-30 21:02:41 -0500

    Switch from String to &str

--- src/lib.rs
+++ src/lib.rs
@@ -11,13 +11,13 @@ mod protocol;
 
 pub fn example() -> String {
     let req = protocol::DkDamSearchServletRequest {
-        app_ver: "1.2.0".to_string(),
-        device_id: "test".to_string(),
-        device_nm: "hello".to_string(),
-        os_ver: "4.4.4".to_string(),
+        app_ver: "1.2.0",
+        device_id: "test",
+        device_nm: "hello",
+        os_ver: "4.4.4",
 
-        page: "1".to_string(),
-        category_cd: "020000".to_string(),
+        page: "1",
+        category_cd: "020000",
         .. Default::default()
     };
 
--- src/protocol.rs
+++ src/protocol.rs
@@ -1,36 +1,36 @@
 #[derive(Default, Serialize)]
-pub struct DkDamSearchServletRequest {
+pub struct DkDamSearchServletRequest<'a> {
     #[serde(rename = "appVer")]
-    pub app_ver: String,
+    pub app_ver: &'a str,
     #[serde(rename = "deviceId")]
-    pub device_id: String,
+    pub device_id: &'a str,
     #[serde(rename = "deviceNm")]
-    pub device_nm: String,
+    pub device_nm: &'a str,
     #[serde(rename = "osVer")]
-    pub os_ver: String,
+    pub os_ver: &'a str,
 
     #[serde(rename = "page")]
-    pub page: String,
+    pub page: &'a str,
     #[serde(rename = "categoryCd")]
-    pub category_cd: String,
+    pub category_cd: &'a str,
 
     #[serde(rename = "serialNo", skip_serializing_if = "Option::is_none")]
-    pub serial_no: Option<String>,
+    pub serial_no: Option<&'a str>,
 
     #[serde(rename = "artistId", skip_serializing_if = "Option::is_none")]
-    pub artist_id: Option<String>,
+    pub artist_id: Option<&'a str>,
 
     #[serde(rename = "artistName", skip_serializing_if = "Option::is_none")]
-    pub artist_name: Option<String>,
+    pub artist_name: Option<&'a str>,
     #[serde(rename = "artistMatchType", skip_serializing_if = "Option::is_none")]
-    pub artist_match_type: Option<String>,
+    pub artist_match_type: Option<&'a str>,
 
     #[serde(rename = "songName", skip_serializing_if = "Option::is_none")]
-    pub song_name: Option<String>,
+    pub song_name: Option<&'a str>,
     #[serde(rename = "songMatchType", skip_serializing_if = "Option::is_none")]
-    pub song_match_type: Option<String>,
+    pub song_match_type: Option<&'a str>,
 
     #[serde(rename = "programTitle", skip_serializing_if = "Option::is_none")]
-    pub program_title: Option<String>
+    pub program_title: Option<&'a str>
 }
 
