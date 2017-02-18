0fa7b897fc040c7a0bd08b137c2dfe0d24a5053d
Walfie <walfington@gmail.com>
2017-02-18 00:00:36 -0500

    Add artists subcommand

--- clubdarn-cli/src/main.rs
+++ clubdarn-cli/src/main.rs
@@ -9,7 +9,7 @@ extern crate serde_json;
 
 pub mod error;
 
-use clap::{AppSettings, Arg, SubCommand};
+use clap::{AppSettings, Arg, ArgGroup, SubCommand};
 use error::*;
 use serde::Serialize;
 
@@ -41,45 +41,67 @@ fn run() -> Result<()> {
             .help("Unique ID for karaoke machine (e.g., AB316238)")
             .short("s")
             .long("serial-no")
+            .value_name("SERIAL_NO")
             .takes_value(true)
             .global(true))
         .arg(Arg::with_name("page")
             .help("Page number for pagination")
             .short("p")
             .long("page")
+            .value_name("PAGE_NUMBER")
             .takes_value(true)
             .default_value("1")
             .global(true))
         .arg(Arg::with_name("app_ver")
             .help("appVer sent to ClubDAM API")
             .long("app-ver")
+            .value_name("APP_VERSION")
             .takes_value(true)
             .default_value(default_metadata.app_ver)
             .global(true))
         .arg(Arg::with_name("device_id")
             .help("deviceId sent to ClubDAM API")
             .long("device-id")
+            .value_name("DEVICE_ID")
             .takes_value(true)
             .default_value(default_metadata.device_id)
             .global(true))
         .arg(Arg::with_name("device_nm")
             .help("deviceNm sent to ClubDAM API")
             .long("device-nm")
+            .value_name("DEVICE_NAME")
             .takes_value(true)
             .default_value(default_metadata.device_nm)
             .global(true))
         .arg(Arg::with_name("os_ver")
             .help("osVer sent to ClubDAM API")
             .long("os-ver")
+            .value_name("OS_VERSION")
             .takes_value(true)
             .default_value(default_metadata.os_ver)
             .global(true))
         .subcommand(SubCommand::with_name("series")
-            .about("List various series by category ID")
+            .about("List series by category ID")
             .arg(Arg::with_name("category_id")
                 .required(true)
                 .index(1)
+                .value_name("CATEGORY_ID")
                 .possible_values(&all_categories)))
+        .subcommand(SubCommand::with_name("artists")
+            .about("Find artists")
+            .group(ArgGroup::with_name("filter")
+                .required(true)
+                .args(&["starts_with", "contains"]))
+            .arg(Arg::with_name("starts_with")
+                .help("Find artist names starting with <QUERY>")
+                .long("starts-with")
+                .value_name("QUERY")
+                .takes_value(true))
+            .arg(Arg::with_name("contains")
+                .help("Find artist names containing <QUERY>")
+                .long("contains")
+                .value_name("QUERY")
+                .takes_value(true)))
         .get_matches();
 
     let metadata = clubdarn::Metadata {
@@ -96,19 +118,34 @@ fn run() -> Result<()> {
 
     let page = value_t!(matches, "page", i32)?;
 
-    let category_id: String;
+    let query: String;
 
-    let mut builder = match matches.subcommand() {
+    // TODO: Put these in separate methods
+    match matches.subcommand() {
         ("series", Some(matches)) => {
-            category_id = value_t!(matches, "category_id", String)?;
-            client.series().by_category_id(&category_id)
+            query = value_t!(matches, "category_id", String)?;
+            let result = client.series().by_category_id(&query).set_page(page).send()?;
+
+            pretty_print(&result)?;
+        }
+        ("artists", Some(matches)) => {
+            use clubdarn::MatchType::{StartsWith, Contains};
+            let artists = client.artists();
+
+            let result = if let Some(q) = matches.value_of("starts_with") {
+                    artists.starting_with(q)
+                } else if let Some(q) = matches.value_of("contains") {
+                    artists.containing(q)
+                } else {
+                    Err("Unknown state")?
+                }.set_page(page)
+                .send()?;
+
+            pretty_print(&result)?;
         }
         (other, _) => Err(format!("Invalid command {}", other))?,
     };
 
-    let result = builder.set_page(page).send()?;
-
-    pretty_print(&result)?;
     Ok(())
 }
 
